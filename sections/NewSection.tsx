915a566d8f5766f8253aac260359c6cf4570ee1a
Fabio Fontoura <fabio@n49.com.br>
2024-06-29 17:21:38 +0000

    Release staging environment with 5 changes made by Fabio Fontoura

---
+++ sections/NewSection.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  /**
+  * @description The description of name.
+  */
+  name?: string;
+}
+
+export default function Section({ name = "Capy" }: Props) {
+  return <div><script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
+    <div class="elfsight-app-1d9c085a-00a7-42ca-ac92-09fb2995b410" data-elfsight-app-lazy></div></div>
+}
